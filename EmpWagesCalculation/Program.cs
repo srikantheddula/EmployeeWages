889b987927eea4370c74e70e9027835906cedd69
srikantheddula <you@example.com>
2022-01-21 16:51:34 +0530

    [srikantheddula]: Added Welcome Message EmployeeWage

---
+++ EmpWagesCalculation/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EmpWagesCalculation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To EmpWagesCalculations");
+        }
+    }
+}
