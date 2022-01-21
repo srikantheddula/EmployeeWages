228cd0c136767c57a6bb2783b88498d338597955
srikantheddula <you@example.com>
2022-01-21 18:14:00 +0530

    [srikantheddula]: Added Employewage5 calulation problem

--- EmpWagesCalculation/EmployeeWagesProblem.cs
+++ EmpWagesCalculation/EmployeeWagesProblem.cs
@@ -5,40 +5,49 @@ using System.Text;
 namespace EmpWagesCalculation
 {
     /// <summary>
-    /// EmployeeWage Calculation
+    ///  //Caluculating Wages assuming 20 Working Days in a Month using forloop
     /// </summary>
     public class EmployeeWagesProblem
     {
-        public static void EmployWage()   //Static method Calculating Empwage using Switch case 
+        const int IS_FULL_TIME = 1;
+        const int IS_PART_TIME = 2;
+        const int EMP_RATE_PER_HOUR = 10;
+        const int NUM_OF_WORKING_DAYS = 20;
+        public static void EmployWage()   //Static method Calculating Empwage 
         {
-            const int IS_FULL_TIME = 1;
-            const int IS_PART_TIME = 2;
-            const int EMP_RATE_PER_HOUR = 10;
            
-                int empHrs = 0;
-                int empWage = 0;
-                Random randomNumber = new Random();
+            int empHrs = 0;
+            int empWage = 0;
+            int totalEmpWage = 0;
+
+            Random randomNumber = new Random();
+            for (int day = 1; day <= NUM_OF_WORKING_DAYS; day++)
+            {
                 int checkingNum = randomNumber.Next(0, 3);
                 switch (checkingNum)
                 {
                     case IS_FULL_TIME:
                         empHrs = 8;
-                        Console.WriteLine("Emp present FullTime");
+                        Console.WriteLine("\nEmp present FullTime");
                         break;
                     case IS_PART_TIME:
                         empHrs = 4;
-                        Console.WriteLine("Emp present partTime");
+                        Console.WriteLine("\nEmp present partTime");
 
                         break;
                     default:
                         empHrs = 0;
-                        Console.WriteLine("Emp absent");
+                        Console.WriteLine("\nEmp absent");
 
                         break;
                 }
                 empWage = empHrs * EMP_RATE_PER_HOUR;
-                Console.WriteLine("Emp Wage: \n" + empWage);
+                totalEmpWage = totalEmpWage + empWage;
+                Console.WriteLine("daycount:\t" + day + "\nEmp Wage:\t" + empWage);
+                Console.WriteLine("----------------------------------------");
             }
+            Console.WriteLine("\n\nTotal Employee wage :\t" + totalEmpWage);
+        }
         }
 }
  
--- EmpWagesCalculation/Program.cs
+++ EmpWagesCalculation/Program.cs
@@ -2,7 +2,7 @@
 
 namespace EmpWagesCalculation
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
